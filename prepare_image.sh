3cf450046f6457d4822985069f7491a051d64a91
Julian Pawlowski <julian.pawlowski@gmail.com>
2013-03-18 14:07:53 +0100

    path corrections

--- prepare_image.sh
+++ prepare_image.sh
@@ -2,10 +2,10 @@
 
 . GPiBE.conf
 
-IMAGE_ARCHIVE_FILE="./cache/${RPI_IMAGE_SRC_URL##*/}"
+IMAGE_ARCHIVE_FILE="cache/${RPI_IMAGE_SRC_URL##*/}"
 IMAGE_FILE="${IMAGE_ARCHIVE_FILE%%.*}.img"
-MNT="./tools/mnt-pi-img.sh"
-GPI_IMAGE="./images/gs5-rpi.img"
+MNT="tools/mnt-pi-img.sh"
+GPI_IMAGE="images/gs5-rpi.img"
 
 cd $(dirname $(readlink -f $0))
 
@@ -19,18 +19,18 @@ cd $(dirname $(readlink -f $0))
 [ ! -e "${GPI_IMAGE}" ] && cp "${IMAGE_FILE}" "${GPI_IMAGE}"
 
 # Mount
-${MNT} "${GPI_IMAGE}" ./chroot
-mkdir -p ./chroot/be
-mount -o bind ./ ./chroot/be
+${MNT} "${GPI_IMAGE}" chroot
+mkdir -p chroot/be
+mount -o bind ./ chroot/be
 
 # Shrink image
-chroot ./chroot apt-get --yes purge $(cat /be/package-lists/dpkg.cleanup)
-chroot ./chroot rm -rf /usr/lib/xorg/modules/linux /usr/lib/xorg/modules/extensions /usr/lib/xorg/modules /usr/lib/xorg
-chroot ./chroot apt-get --yes autoremove
-chroot ./chroot apt-get --yes autoclean
-chroot ./chroot apt-get --yes clean
+chroot chroot apt-get --yes purge $(cat /be/package-lists/dpkg.cleanup)
+chroot chroot rm -rf /usr/lib/xorg/modules/linux /usr/lib/xorg/modules/extensions /usr/lib/xorg/modules /usr/lib/xorg
+chroot chroot apt-get --yes autoremove
+chroot chroot apt-get --yes autoclean
+chroot chroot apt-get --yes clean
 
 # umount
-${MNT} -u ./chroot
+${MNT} -u chroot
 
 cd - 2>&1>/dev/null
