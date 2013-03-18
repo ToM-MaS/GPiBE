7c976191c45df859fb3302f49c6aad117a398342
Julian Pawlowski <julian.pawlowski@gmail.com>
2013-03-18 15:40:56 +0100

    package management

--- hooks/01_packages.sh
+++ hooks/01_packages.sh
@@ -1,12 +1,11 @@
 #!/bin/bash -e
 
 # Copy archives from GBE Debian Live repo
-for FILE in /be/upstream/GBE/config.v3/archives/*.list.chroot; do
+for FILE in /be/archives/*.list.chroot; do
 	DEST_FILE="/etc/apt/sources.list.d/`basename "${FILE%%.*}"`"
 	cp -f "${FILE}" "${DEST_FILE}"
-	sed -i 's/debian/raspbian/' "${DEST_FILE}"
 done
-find /be/upstream/GBE/config.v3/archives -type f -name "*.key.chroot" -exec apt-key add {} \;
+find /be/archives -type f -name "*.key.chroot" -exec apt-key add {} \;
 
 #FIXME
 echo "89.221.14.194 repo.amooma.de" >> /etc/hosts
@@ -21,6 +20,10 @@ for FILE in /be/upstream/GBE/config.v3/preseed/*.cfg.chroot; do
 done
 
 # Install packages
+for FILE in /be/package-lists/*.list.chroot; do
+	apt-get --yes install $(cat ${FILE} | grep -Ev ^# | grep -Ev "^$")
+done
 for FILE in /be/upstream/GBE/config.v3/package-lists/*.list.chroot; do
+	[ "${FILE##*/}" == "01-gdfdl_system.list.chroot" || "${FILE##*/}" == "02-gemeinschaft_system.list.chroot" ] && continue
 	apt-get --yes install $(cat ${FILE} | grep -Ev ^# | grep -Ev "^$")
 done
